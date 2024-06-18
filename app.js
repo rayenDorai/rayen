3cb9d2194ac198c5fc4e8f09426acb12c2f2283a
rayenDorai <rayendorai69@hotmail.com>
2024-06-18 02:36:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     var password = document.getElementById("password").value;
   
     if (password === "r1x22r36") {
-      window.location.href = "https://www.youtube.com/"; // Redirect to success page or do something on successful login
+      window.location.href = "https://rayendorai.github.io/11/"; // Redirect to success page or do something on successful login
     } else {
       document.getElementById("message").innerText = "Incorrect password. Please try again.";
     }
